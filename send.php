bd9cf0229ec27ed82e656137eb51516e1d4dcfbe
Charles Sá <felizardocharles@hotmail.com>
2019-08-12 15:29:39 -0300

    Update send.php

--- send.php
+++ send.php
@@ -4,9 +4,8 @@ require_once 'config.php';
 /**
  * OS DADOS ABAIXO DEVEM SER ENVIADOS PELA APLICAÇÃO
  */
-/*
 $merchantOrderNumber = $_GET['id_pedido'];
-$produto = $_GET['nome_produto'];
+$nome_produto = $_GET['nome_produto'];
 $descricao_produto_opcional = isset($_GET['descricao_produto']) ? $_GET['descricao_produto'] : '';
 $valor = $_GET['valor']; // em centavos
 $nome_comprador = $_GET['nome_comprador'];
@@ -17,30 +16,18 @@ $url_retorno = isset($_GET['url_retorno']) ? $_GET['url_retorno'] : 'www.google.
 // Define para qual url o comprador será enviado após finalizar a compra.
 // Uma URL fixa pode ser registrada no Backoffice Checkout, porém é desconsiderada
 // caso seja enviado por esse campo.
- *
- */
-
-$merchantOrderNumber = 1234;
-$produto = 'Produto de Teste';
-$descricao_produto_opcional = '';
-$valor = 10000;
-$nome_comprador = 'FULANO DE TAL';
-$email_comprador = 'aaa@hotmail.com.br';
-$fone_comprador ='88888888888';
-$doc_comprador = '11111111111';
-$url_retorno = 'www.google.com.br';
-
 
 $cabecalhos[] = array('Cache-Control: no-cache','Content-Type: application/json','MerchantId: ' . MID);
 
 $produtos[] = array(
-    'Name' => $produto,
+    'Name' => $nome_produto,
     'Description' => $descricao_produto_opcional,
     'UnitPrice' => $valor,
     'Type' => 'Service',
     'Quantity' => 1
 );
 
+// Não testei o uso da recorrência de pagamento
 //$recorrencia[] =array(
 //    'Interval' => 'Monthly',
 //    'EndDate' => 'YYYY-MM-DD'
@@ -69,7 +56,7 @@ $dados = array(
         'Identity' => $doc_comprador
     ),
     'Options'=>array(
-        'AntifraudEnabled' => FALSE,
+        'AntifraudEnabled' => FALSE, // Não testei o uso da Anti-Fraude
         'ReturnUrl' => $url_retorno
     )
 );
@@ -92,12 +79,13 @@ if (json_last_error()) {
 
     $response = json_decode($response);
 
+    // Considerar somente o primeiro índice (só haverá mais de um em caso de erro).
     if(is_array($response)){
         $response = $response[0];
     }
 
     if (isset($response->Message)) {
-        print 'Erro ao conectar com a Cielo: '.$response->Message;
+        echo 'Erro ao conectar com a Cielo: '.$response->Message;
     } else {
         $checkoutUrl = $response->settings->checkoutUrl;
 
@@ -105,7 +93,7 @@ if (json_last_error()) {
             echo "<script>window.location='{$checkoutUrl}';</script>";
             exit();
         } else {
-            print 'Erro ao gerar link de carrinho da Cielo!';
+            echo 'Erro ao gerar link de carrinho da Cielo!';
         }
     }
 }
