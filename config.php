4b4b273c249b56c5c61696a05c4dec6cb8e7ab18
Charles Sá <felizardocharles@hotmail.com>
2019-08-12 15:24:27 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,4 @@
  */
 define('ENDPOINT', 'https://cieloecommerce.cielo.com.br/api/public/v1/orders');
 define('MID', 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'); // Obter com a Cielo
-define('MNAME', 'IADEUVA'); // Texto exibido na fatura do comprador. Sem caracteres especiais ou espaços.
+define('MNAME', 'SEU_CLIENTE'); // Texto exibido na fatura do comprador. Sem caracteres especiais ou espaços.
